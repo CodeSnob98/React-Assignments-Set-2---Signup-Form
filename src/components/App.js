32b7c4359069624e1da9e9c9d1e4bedaa32bb520
CodeSnob98 <63289568+CodeSnob98@users.noreply.github.com>
2020-12-04 16:47:11 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+import Login from "./login";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
-
+    <div id="main">
+      <Login />
+    </div>
+  );
+};
 export default App;
